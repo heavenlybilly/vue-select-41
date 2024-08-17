944fb3fab2897b9c854a4ac070c0bd40bc1b088b
heavenlybilly <heavenlybilly@gmail.com>
2024-08-17 15:03:44 +0300

    test: add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   testEnvironment: "node",
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    "^.+.tsx?$": ["ts-jest",{}],
+    '^.+\\.tsx?$': ['ts-jest', {}],
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,vue}',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/main.ts',
+    '!src/App.vue',
+  ],
 };
